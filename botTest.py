7fa9dcbea5ca5d6deb5ed9a25c692be68582e85a
Rv-ben <rubenbramasco@gmail.com>
2020-03-20 20:13:37 -0700

    Basic working bot

--- botTest.py
+++ botTest.py
@@ -1,11 +1,11 @@
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+client = commands.Bot(command_prefix = 'MC')
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run('my token goes here')
+@client.command()
+async def test(ctx):
+    await ctx.send('This is a test')
+
+client.run('')
